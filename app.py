c114f19d2038313f439f50275d8ac015db105680
Alifah25 <alifaqil2506@gmail.com>
2024-03-23 13:29:25 +0700

    freeze dotenv and other requirements

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import os
 from os.path import join, dirname
 from dotenv import load_dotenv
 
+from blinker import signal
+
 from flask import Flask, render_template, request, jsonify
 from pymongo import MongoClient
 
